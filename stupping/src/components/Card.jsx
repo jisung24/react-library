b469cf8f38b8b10f408cdf740c3c1f5d5905530a
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-10-20 01:01:43 +0900

    props객체를 통해 component의 차별되는 data를 주입해주고,컴포넌트의 재사용성을 한결 더 높임

---
+++ stupping/src/components/Card.jsx
@@ -0,0 +1,29 @@
+// 카드주인의 이름, 직업을 state로 변하게 만들어놓고 사용하면
+// 컴포넌트의 재사용성이 훨씬 더 높아지겠지????  
+
+function UserCard({ name, age }){
+    // let { name , age } = props객체...!! 
+    // props는 객체 형태로 전달이 되며, 그 속성에 접근하고 싶으면
+    // js destructure문법으로 접근하면 된다.!! 
+
+    // props전달하기!! 
+    // 이 컴포넌트를 사용하는 곳에서 정의해줘야 한다. 
+    // 와 이거 진짜 이해 잘 된다.... props를 이용하자!! 
+
+    // 차별되는 data => props로 변경해주면 돼! 
+    return (
+        <div className="user__card">
+            <div className="user__image"></div>
+            <div className="user__profile">
+                <h1 className="user__name">
+                    { name }
+                </h1>
+                <p className="user__age">
+                    { age }
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard;
--- stupping/src/components/Main.jsx
+++ stupping/src/components/Main.jsx
@@ -1,10 +1,18 @@
 // 여기서는 컴포넌트를 가장 작은 단위로 하나하나 만드는 역할! 
 
-// 이제 각각 만든 컴포넌트를 export시켜서 App.js에 보내는 형태야!  
+// 이제 각각 만든 컴포넌트를 export시켜서 App.js에 보내는 형태야! 
+
+import UserCard from "./Card";
+
 function Main(){
+
     return (
         <main className="main">
-            Main
+            <UserCard name="김지성" age="20"/>
+            <UserCard name="장종운" age="21" />
+            <UserCard name="이재혁" age="22" />
+            <UserCard name="조홍래" age="23" />
+            <UserCard name="박현태" age="24" />
         </main>
     )
 }
