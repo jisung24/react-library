24cb251983e65917fc4e8eac72ebfceae2e22183
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-10-21 22:44:18 +0900

    리엑트 코드 복습파일 만들어서 공부

--- stupping/src/components/Name.jsx
+++ stupping/src/components/Name.jsx
@@ -1,5 +1,6 @@
 // 우리가 사용하다보니까 카드 안에있는 name도 컴포넌트로 재사용하고싶어!!
-function Name({ name }){
+function Name({ name }){ // 객체로 들어감...!! props객체 안에 name속성이 들어감. 
+    // name값은 이 컴포넌트를 사용하는 쪽에서 정의를 해 준다. 
 
     return (
         <h1 className="user__name">
